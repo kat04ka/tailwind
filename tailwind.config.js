4f0c6b1abca71891df03d3c317e9055e9011339b
kat04ka <fcmlexa@gmail.com>
2026-02-13 23:16:55 +0200

    refactor: convert section steps to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
         ],
 
         "heading-1": [
-          "clamp(32px, 6cqw, 60px)",
+          "clamp(50px, 6cqw, 60px)",
           {
             lineHeight: "0.9",
             letterSpacing: "-0.03em",
